339439e3a333f1affc0091d2ca9e0a137d4a1156
Juan Vaca <jvaca@advlatam.com>
2017-02-22 12:20:30 -0500

    Mejoras
    Se mejoran los mensajes
    Se Pregunta al usuario si desea continuar

--- jboss.4.2/shutdown.sh
+++ jboss.4.2/shutdown.sh
@@ -9,12 +9,22 @@ export PATH=$JAVA_HOME/bin:$PATH
 #echo JAVA_HOME=${JAVA_HOME}
 #echo PATH=${PATH}
 
-echo "Este comando ejecuta una finalización segura del servidor Jboss.  El proceso puede tardar varios minutos dependiendo de la carga que tenga el servidor"
-error_count=`${JBOSS_DIR}/bin/shutdown.sh -S 2>&1>/dev/null | grep 'Failed to connect to server' | wc -l`
+echo "Este comando ejecuta una finalización segura del servidor Jboss 4.2.2"
+echo "El proceso puede tardar varios minutos dependiendo de la carga que tenga el servidor"
+echo -n "Desea bajar jboss 4.2.2 levantado en este servidor S/N? [N]: "
+read _continue
 
-if [ $error_count -eq 0 ] 
-then 
-    echo "Se ha solicitado la finalización del proceso del servidor jboss ${JBOSS_DIR}. El proceso de finalización puede tardar varios minutos.  Verifique el resultado de esta solicitud en el log del servidor"
+if [[ "$_continue" == *[Ss]* ]]; then
+    error_count=`${JBOSS_DIR}/bin/shutdown.sh -S 2>&1>/dev/null | grep 'Failed to connect to server' | wc -l`
+
+    if [ $error_count -eq 0 ] 
+    then 
+        echo "Se ha solicitado la finalización del proceso del servidor jboss ${JBOSS_DIR}."
+        echo "El proceso de finalización puede tardar varios minutos."
+        echo "Verifique el resultado de esta solicitud en el log del servidor."
+    else
+        echo "Jboss no se encontraba en ejecución, no se realizó ninguna acción"
+    fi
 else
-    echo "Jboss no se encuentra en ejecución"
+    echo "No se ha realizado ninguna acción."
 fi
